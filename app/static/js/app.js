81543adf3b7da19659720a7e19e98ed8537a50c6
Junior Kossivi <junioragbenonzan31@gmail.com>
2026-03-20 18:50:20 +0000

    Mise en place de la stack technique

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,9 @@
-﻿console.log('App loaded');
+document.addEventListener('DOMContentLoaded', () => {
+    const revealItems = document.querySelectorAll('.reveal-on-load');
+
+    revealItems.forEach((item, index) => {
+        window.setTimeout(() => {
+            item.classList.add('is-visible');
+        }, 70 * index);
+    });
+});
